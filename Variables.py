f1b344791c9f4a92ab17e268f66addd1e93bce20
virendramali <virendravmali77@gmail.com>
2021-04-20 16:17:33 +0530

    first assignment

---
+++ Variables.py
@@ -0,0 +1,40 @@
+a="Hello World!!"
+print(a)
+print(type(a))
+
+x=[1,2,3,4,5,6]
+print(x.pop())
+
+a_b="vicky"
+print(a_b)
+
+#string
+message="Hello Python"
+print(message)
+
+name="Virendra Mali"
+print(name)
+print(type(a))
+
+x="I told my friend, 'Python' is my favourite language"
+print(x)
+
+name="virendra mali"
+print(name.title())	#titlecase
+print(name.upper())	#uppercase
+print(name.lower())	#lowercase
+
+first_name='virendra'
+last_name='mali'
+full_name=first_name +" "+ last_name
+print(full_name.title())					#combining(concatenation)_with_titlecase
+print("Hello, "+full_name.title()+'.')
+message="Hello,"+full_name.title()+"."
+print(message)
+
+print("Sublime text")
+print("\tSublime text")						#add_tab_to_text
+print("Languages:\nPython\nJavascript\nC")	#add_newline_to_string
+
+print("Languages:\n\tPython\n\tJavascript\n\tC")	#combine_tab_and_newline
+
